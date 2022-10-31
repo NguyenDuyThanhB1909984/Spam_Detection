54ea0874342c358e4ff2ba99b5cefaadc1396bd9
Duy_Thanh <thanh08052001@gmail.com>
2022-10-31 21:33:23 +0700

    comment code

--- frontend.py
+++ frontend.py
@@ -1,3 +1,6 @@
+# File phía client dùng để phân loại email
+
+
 from cProfile import label
 from urllib import response
 import streamlit as st
@@ -5,20 +8,23 @@ import requests
 import json
 import pandas as pd
 
-
+# link hoạt động của api đã viết ở file main.py
 url = 'http://localhost:8000/prediction'
 
+# Header của website
 st.header("Spam detection")
 
+
+# Nhãn của email
 label = ['Spam email', 'non spam']
 
-
+# Tạo form để người dùng nhập, gởi dữ liệu phân loại email 
 with st.form(key="predict_spam_email",clear_on_submit=True):
     data_email = st.text_input("Subject and body of the mail: ")
     submited = st.form_submit_button("Predict")
    
 
-
+# Gởi dữ liệu lên api để xử lý
 if submited:
     data = {"data": data_email}
     res = requests.post(url,json= data)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# server api nhận dạng mail là spam hay ko 
+
 from typing import Union
 import uvicorn
 import pickle
@@ -6,23 +8,29 @@ from pydantic import BaseModel
 import numpy as np
 
 
+# Load model RandomForestClassifier đã đc huấn luyện để phân lớp  mail
 loaded_model = pickle.load(open('./RandomForestClassifier.sav', 'rb'))
 
+
+# data là nội dung mail người dùng tập từ phía client 
 class Data(BaseModel):
     data: str
    
 
-
+# Gọi FastApi để viết file api
 app = FastAPI()
 
+
+# hàm đc gọi khi client truy cập đến trang chủ
 @app.get("/")
 def read_root():
     return {"message": "Welcome from the API"}
 
-
+# nhận dữ liệu từ cilent
 @app.post("/prediction/")
 async def predict_item(item: Data):
 
+    # Gọi  model đã load để dự đoán 
     result =int(loaded_model.predict([item.data]))
     
     return  result
